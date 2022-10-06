e1615c987aee8ba758e7cd0f60c9157f6d4a6150
Suraj Kumar <ravi.suraj110@gmail.com>
2022-10-06 08:56:53 +0530

    initial setup

---
+++ lib/my_blog/application.ex
@@ -0,0 +1,36 @@
+defmodule MyBlog.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      MyBlog.Repo,
+      # Start the Telemetry supervisor
+      MyBlogWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: MyBlog.PubSub},
+      # Start the Endpoint (http/https)
+      MyBlogWeb.Endpoint
+      # Start a worker by calling: MyBlog.Worker.start_link(arg)
+      # {MyBlog.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MyBlog.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    MyBlogWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/my_blog/mailer.ex
@@ -0,0 +1,3 @@
+defmodule MyBlog.Mailer do
+  use Swoosh.Mailer, otp_app: :my_blog
+end
